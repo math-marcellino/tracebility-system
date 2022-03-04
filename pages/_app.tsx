340d1dff2a95c26ea8521b48593a106af3f3cd11
Matthew Marcellino <matthew.marcellino1123@gmail.com>
2022-03-04 12:23:57 +0700

    Wrap Component with Wagmi Provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,15 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Provider } from 'wagmi'
+import { providers } from 'ethers'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const mumbaiProvider = new providers.JsonRpcProvider('https://speedy-nodes-nyc.moralis.io/0f6aa643f70545beebc8e4f9/polygon/mumbai', 80001)
+  return <>
+    <Provider autoConnect provider={mumbaiProvider}>
+      <Component {...pageProps} />
+    </Provider>
+  </>
 }
 
 export default MyApp
