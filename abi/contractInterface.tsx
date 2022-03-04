417df172f8cb1bd344556a8dff539371cc06b4ff
Matthew Marcellino <matthew.marcellino1123@gmail.com>
2022-03-04 11:54:31 +0700

    Add Smart Contract Javascript Interface

---
+++ abi/contractInterface.tsx
@@ -0,0 +1,15 @@
+import { Interface } from "ethers/lib/utils";
+
+const contractInterface = new Interface([
+    // Read function
+    "function isOwner()",
+
+    // Write function
+    "function createItem(string _name, string _verifier, bool _halal)",
+    "function step2(uint _itemID, string _verifier, bool _halal)",
+    "function step3(uint _itemID, string _verifier, bool _halal)",
+    "function step4(uint _itemID, string _verifier, bool _halal)",
+
+    // Events
+    "event Trace(uint _itemID, uint _step, bool _halal, string verifiers, uint _time)"
+])
