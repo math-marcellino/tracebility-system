58f41bc3c56eed51bd8c4f00aa0750f1e1041524
William <79161142+Wil-lab07@users.noreply.github.com>
2022-03-05 09:36:18 +0700

    fix: adding the styling in form

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,13 +2,16 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { Provider } from 'wagmi'
 import { providers } from 'ethers'
+import {ChakraProvider} from '@chakra-ui/react'
 
 function MyApp({ Component, pageProps }: AppProps) {
   const mumbaiProvider = new providers.JsonRpcProvider('https://speedy-nodes-nyc.moralis.io/0f6aa643f70545beebc8e4f9/polygon/mumbai', 80001)
   return <>
+  <ChakraProvider>
     <Provider autoConnect provider={mumbaiProvider}>
       <Component {...pageProps} />
     </Provider>
+  </ChakraProvider>
   </>
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from 'next'
 import { ethers } from 'ethers'
 import { useContract, useProvider, useContractWrite, useContractRead } from 'wagmi'
+import {Box, Flex, Text, Button, Img, HStack, Stack, InputGroup, Input, Select} from '@chakra-ui/react'
 import { contractABIInterface } from '../abi/contractInterface'
 import { SubmitHandler, useForm } from 'react-hook-form'
 
@@ -61,30 +62,46 @@ const Home: NextPage = () => {
   }
 
   return (
-    <div>
-      <form onSubmit={handleSubmit(submitData)}>
-        <div style={{display: "flex", justifyContent: "space-between"}}>
-          <label htmlFor="">Nama Makanan</label>
-          <input {...register('_nama')} type={'text'}></input>
-        </div>
-        <br />
-        <div style={{display: "flex", justifyContent: "space-between"}}>
-          <label>Pilih Kehalalan:</label>
-          <select {...register('_halal')}>
-            <option value="true">Halal</option>
-            <option value="false">Non Halal</option>
-          </select>
-        </div>
-        <br />
-        <div style={{display: "flex", justifyContent: "space-between"}}>
-          <label htmlFor="">Verifier</label>
-          <input {...register('_verifier')} type={'text'}></input>
-        </div>
-        <div style={{display: "flex", justifyContent: "space-between"}}>
-          <button type='submit'>Submit Data</button>
-        </div>
-      </form>
-    </div>
+    <Flex height={'100vh'} justify='center' align={'center'}>
+      <Flex justify={'center'} align={'center'}>
+        <Flex
+          borderRadius={30}
+          bgColor="#191B1F"
+          w="30rem"
+          py={4}
+          pb={8}
+          px={6}
+          direction={'column'}
+        >
+          <Stack spacing={5}>
+            <Flex flex={{base: 1}} justify={'space-between'} align={'center'}>
+              <Text>Input Data</Text>
+            </Flex>
+            <form autoComplete="off" onSubmit={handleSubmit(submitData)}>
+              <Stack spacing={5}>
+                <Flex flex={{base: 1}} justify={'space-between'} pr={3} align={'center'} bgColor="#20242A" borderRadius={20}>
+                  <InputGroup>
+                    <Input textColor="#B2B9D2" type="number" border={'none'} step={'any'} focusBorderColor={'none'} placeholder="Nama Makanan" min={0} {...register('_nama')}></Input>
+                  </InputGroup>
+                </Flex>
+                <Flex flex={{base: 1}} justify={'space-between'} pr={3} align={'center'} bgColor="#20242A" borderRadius={20}>
+                  <Select placeholder='Status Kehalalan' border={'none'} {...register('_halal')}>
+                    <option value='true'>Halal</option>
+                    <option value='false'>Non Halal</option>
+                  </Select>
+                </Flex>
+                <Flex flex={{base: 1}} justify={'space-between'} pr={3} align={'center'} bgColor="#20242A" borderRadius={20}>
+                  <InputGroup>
+                    <Input textColor="#B2B9D2" type="text" border={'none'} focusBorderColor={'none'} placeholder={'Pemverifikasi'} {...register('_verifier')}></Input>
+                  </InputGroup>
+                </Flex>
+                <Button colorScheme={'blue'} type="submit" borderRadius={20}>Confirm</Button>
+              </Stack>
+            </form>
+          </Stack>
+        </Flex>
+      </Flex>
+    </Flex>
   )
 }
 
