38c6cb7ac20c0ff4438e342e56547c436a381ea6
Matthew Marcellino <matthew.marcellino1123@gmail.com>
2022-03-16 14:49:10 +0700

    fix: change returned data in useTraceEvents hook

--- hooks/useTraceEvents.tsx
+++ hooks/useTraceEvents.tsx
@@ -7,6 +7,14 @@ export type TraceEventsRequest = {
     provider: ethers.providers.BaseProvider;
 }
 
+export type TraceEventsResult = {
+    date: string,
+    itemID: number,
+    step: number,
+    verifiers: string,
+    halal: boolean;
+}
+
 const TraceEventsFetcher = (args: TraceEventsRequest): Promise<Array<Event>> => {
     const tracebilityContract = new ethers.Contract(args.contractAddress, contractABIInterface, args.provider);
 
@@ -25,8 +33,21 @@ export function useTraceEvents(req: TraceEventsRequest){
         TraceEventsFetcher
     );
     
+    const result:Array<TraceEventsResult> = data! && data?.map((item) => {
+        const date = new Date(item.args?._time.toNumber() * 1000)
+        const formattedDate = new Intl.DateTimeFormat("en-US", { hour: "numeric", day: "numeric", month: "short", year: "numeric", minute: "numeric" }).format(date);
+        const event:TraceEventsResult = {
+            date: formattedDate,
+            itemID: item.args?._itemID.toNumber(),
+            step: item.args?._step.toNumber()+1,
+            verifiers: item.args?.verifiers,
+            halal: item.args?._halal
+        }
+        return event;
+    })
+    
     return { 
-        data: data, 
+        data: result, 
         isLoading: !data && !error,
         error: error
     }
